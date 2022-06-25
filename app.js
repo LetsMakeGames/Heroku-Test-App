d213910780e9a2447359145c7c9569f9a9b75481
William Barnes <jakebarnes1989@gmail.com>
2022-06-25 16:25:55 -0700

    Updated bolt package and installed dotenv for local testing

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+function keywordInMessage(message, next) {
+  const keyword = 'hello';
+  if (message.text.includes(keyword)) {
+    next();
+  }
+};
+
+// A function that runs when a keyword from a list is detected
+app.message(keywordInMessage, async ({ message, context }) => {
+  // Send a message to the channel
+  await context.sendMessage(`You said: ${message.text}`);
+});
+
 (async () => {
   // Start your app
   await app.start(Number(process.env.PORT || 3000));
